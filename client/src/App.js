c008998a3da8223ca03d78b11b811f8b1efed45a
Damian_Henisz <damianh1293@gmail.com>
2019-05-25 15:33:50 +0200

    Added Provider for initial store

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import WelcomeComponent from "./components/WelcomeComponent";
 import LoginComponent from "./components/LoginComponent";
 import RegisterComponent from "./components/RegisterComponent";
 import DocumentComponent from "./components/DocumentComponent";
+
+import { Provider } from "react-redux";
+import store from "./store/store";
 import "./styles/App.scss";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route exact path="/" component={WelcomeComponent} />
-        <Route exact path="/register" component={RegisterComponent} />
-        <Route exact path="/login" component={LoginComponent} />
-        <Route exact path="/document" component={DocumentComponent} />
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Route exact path="/" component={WelcomeComponent} />
+          <Route exact path="/register" component={RegisterComponent} />
+          <Route exact path="/login" component={LoginComponent} />
+          <Route exact path="/document" component={DocumentComponent} />
+        </Router>
+      </Provider>
     );
   }
 }
