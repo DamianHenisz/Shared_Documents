d2f81b4737d0d27c9b8b0cb6d0ba5c39faa70896
Damian_Henisz <damianh1293@gmail.com>
2019-05-27 12:18:30 +0200

    Added logic for setAuthToken

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import WelcomeComponent from "./components/WelcomeComponent";
 import LoginComponent from "./components/LoginComponent";
 import RegisterComponent from "./components/RegisterComponent";
 import DocumentComponent from "./components/DocumentComponent";
 
+import test from "./components/test";
+
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { setCurrentUser } from "./actions/authActions";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 import "./styles/App.scss";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+
+  const decodedToken = jwt_decode(localStorage.token);
+  store.dispatch(setCurrentUser(decodedToken));
+}
 class App extends Component {
   render() {
     return (
@@ -18,6 +29,7 @@ class App extends Component {
           <Route exact path="/" component={WelcomeComponent} />
           <Route exact path="/register" component={RegisterComponent} />
           <Route exact path="/login" component={LoginComponent} />
+          <Route exact path="/test" component={test} />
           <Route exact path="/document" component={DocumentComponent} />
         </Router>
       </Provider>
