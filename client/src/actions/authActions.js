8e9a23331563e733bd336338e56abe575478fb81
Damian_Henisz <damianh1293@gmail.com>
2019-05-27 12:10:20 +0200

    Added Acionss loginUser and setCurrentUser in Redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-
-import { GET_ERRORS } from "./types";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 //Register User
-export const registerUser = (userData, history, isLoader) => dispatch => {
+export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
     .then(res => {
@@ -16,4 +17,37 @@ export const registerUser = (userData, history, isLoader) => dispatch => {
         payload: err.response.data
       });
     });
+};
+
+//Login User
+//Get User Token
+export const loginUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      const token = res.data.token;
+      //Set token to Local Storage
+      localStorage.setItem("token", token);
+      //Set token to header
+      setAuthToken(token);
+      //Decode token to get userData
+      const decodedToken = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decodedToken));
+      history.push("test");
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+//Set Current User
+export const setCurrentUser = decodedToken => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decodedToken
+  };
 };
