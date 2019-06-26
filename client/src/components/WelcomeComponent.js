b2cad76f7557535a977b5f053502275bad826e99
Damian_Henisz <damianh1293@gmail.com>
2019-06-26 14:52:02 +0200

    Added test styles in WelcomeComponent

--- client/src/components/WelcomeComponent.js
+++ client/src/components/WelcomeComponent.js
@@ -3,20 +3,28 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
+import "../styles/WelcomeStyle.scss";
+
 class WelcomeComponent extends Component {
   componentDidMount() {
     if (this.props.auth.isAuthenticated) this.props.history.push("/documents");
   }
   render() {
     return (
-      <div>
-        <p>WelcomeComponent.js</p>
-        <Link to="/register">
-          <button className="btn btn-primary">Zarejestruj Się </button>
-        </Link>
-        <Link to="login">
-          <button className="btn btn-primary">Zaloguj Się </button>
-        </Link>
+      <div className="App">
+        <div className="App-header">
+          <p>WelcomeComponent1.js</p>
+          <Link to="/register">
+            <button className="btn btn-primary">Zarejestruj Się </button>
+          </Link>
+          <Link to="login">
+            <button className="btn btn-primary">Zaloguj Się </button>
+          </Link>
+        </div>
+        <div className="App-header">
+          <p>WelcomeComponent1.js</p>
+          <img src={require("../img/welcome_picture.svg")} className="App-logo" alt="WelcomePicture" />
+        </div>
       </div>
     );
   }
